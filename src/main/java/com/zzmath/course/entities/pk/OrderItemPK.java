fde3835e29c6cfcb66f2dd08e95efc6cf3a2e2f5
zZMathSP <zzmathsp@gmail.com>
2019-11-15 12:41:44 -0400

    Entidade OrderItem. Associação muitos-para-muitos com dados extras - PARTE 1

---
+++ src/main/java/com/zzmath/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,70 @@
+package com.zzmath.course.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.zzmath.course.entities.Order;
+import com.zzmath.course.entities.Product;
+
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.order);
+        hash = 83 * hash + Objects.hashCode(this.product);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final OrderItemPK other = (OrderItemPK) obj;
+        if (!Objects.equals(this.order, other.order)) {
+            return false;
+        }
+        if (!Objects.equals(this.product, other.product)) {
+            return false;
+        }
+        return true;
+    }
+}
