69de20b5ccb33bcdba2c8b9cb941a5dfda985411
zZMathSP <zzmathsp@gmail.com>
2019-11-15 14:10:24 -0300

    OrderItem, many-to-many association with extra attributes - PART1

--- src/main/java/com/zzmath/course/entities/OrderItem.java
+++ src/main/java/com/zzmath/course/entities/OrderItem.java
@@ -1,103 +1,90 @@
 package com.zzmath.course.entities;
 
-
 import java.io.Serializable;
-import java.util.Objects;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.EmbeddedId;
 
 import com.zzmath.course.entities.pk.OrderItemPK;
-
 
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
-    private static final long serialVersionUID = 1L;
-    
-    @EmbeddedId
-    private OrderItemPK id;
-    
-    private Integer quantity;
-    private Double price;
-    
-    public OrderItem() {
-        
-    }
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		
+	}
+	
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
 
-    public OrderItem(Order order, Product prouct, Integer quantity, Double price) {
-        id.setOrder(order);
-        id.setProduct(prouct);
-        this.quantity = quantity;
-        this.price = price;
-    }
-    
-    public Order getOrder() {
-        return id.getOrder();
-    }
-    
-    public void setOrder(Order order) {
-        id.setOrder(order);
-    }
-    
-    public Product getProduct() {
-        return id.getProduct();
-    }
-    
-    public void setProduct(Product product){
-        id.setProduct(product);
-    }
+	public Integer getQuantity() {
+		return quantity;
+	}
 
-    public Integer getQuantity() {
-        return quantity;
-    }
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+	public Double getPrice() {
+		return price;
+	}
 
-    public Double getPrice() {
-        return price;
-    }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 17 * hash + Objects.hashCode(this.id);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final OrderItem other = (OrderItem) obj;
-        if (!Objects.equals(this.id, other.id)) {
-            return false;
-        }
-        return true;
-    }
-
-    public OrderItemPK getId() {
-        return id;
-    }
-
-    public void setId(OrderItemPK id) {
-        this.id = id;
-    }
-    
-    
-    
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
