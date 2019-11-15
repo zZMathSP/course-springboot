83c1320edf014816d76bad0aa66da7b2f206064e
zZMathSP <zzmathsp@gmail.com>
2019-11-15 14:25:19 -0300

    OrderItem, many-to-many association with extra attributes - PART2

--- src/main/java/com/zzmath/course/config/TestConfig.java
+++ src/main/java/com/zzmath/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.zzmath.course.entities.Category;
 import com.zzmath.course.entities.Order;
+import com.zzmath.course.entities.OrderItem;
 import com.zzmath.course.entities.Product;
 import com.zzmath.course.entities.User;
 import com.zzmath.course.entities.enums.OrderStatus;
 import com.zzmath.course.repositories.CategoryRepository;
+import com.zzmath.course.repositories.OrderItemRepository;
 import com.zzmath.course.repositories.OrderRepository;
 import com.zzmath.course.repositories.ProductRepository;
 import com.zzmath.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,5 +74,12 @@ public class TestConfig implements CommandLineRunner{
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
